f5ae1458a6c7a5fce93f074437a81c3e572e3d1e
esteban <pyro_fefa@hotmail.com>
2013-06-18 17:38:07 -0700

    web
    web pages

--- js/index.js
+++ js/index.js
@@ -1,38 +1,5 @@
+// JavaScript Document
 $(document).ready(function (){
-	crearLinks();
-	alert('Cargando DOM');
-	$( "#c" ).show( "drop", 1000 );
+	 $( "#tabs" ).tabs({ show: { effect: "blind", duration: 1000 } });
+	 $( "#accordion" ).accordion();
 });
-function crearLinks() {
-	// Recorro todos los li dentro del Menu
-	var opciones = $("#menu li");
-		// Genero la acción del clic por cada uno
-		opciones.click (function() {
-			// utilizo el id de cada li como nombre de seccion
-			mostrarSeccion(this.id);
-		});
-}
-function mostrarSeccion(nombre) {
-	$.ajax({url: nombre + ".html",
-		cache: true,
-		success:function(respuesta) {
-			//inicio efecto desvanecimiento
-			$("#contenido").fadeOut(function(){
-			$(this)
-			.html(respuesta)// definicion texto
-			.fadeIn();//aparicion
-			});
-		}
-	});
-}
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
-
-
